97c88e1fe201abf2604e8af368bc4d97d52b488e
godavilez <dgavilez@gmail.com>
2018-04-15 21:38:38 -0300

    Implementação stream vogal

---
+++ Stream/Stream/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream
+{
+    class Program
+    {
+        private static List<char> vogaisEncontradasStream = new List<char>();
+
+        static void Main(string[] args)
+        {
+            char value = firstChar(new WordStream());
+
+            if (value != '\0')
+            {
+                Console.WriteLine(value);
+            }
+            else
+            {
+                Console.WriteLine("Não foi encontrado um caracter válido");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static char firstChar(IStream input)
+        {
+            bool ultimoFoiConsoante = false;
+            bool vogalRemovida = false;
+
+            while (input.hasNext())
+            {
+                char caracter = char.ToLower(input.getNext());
+                vogalRemovida = false;
+                switch (caracter)
+                {
+                    case 'a':
+                    case 'e':
+                    case 'i':
+                    case 'o':
+                    case 'u':
+                        if (ultimoFoiConsoante)
+                        {
+                            // É uma vogal agora
+                            ultimoFoiConsoante = false;
+
+                            for (int i = 0; i < vogaisEncontradasStream.Count; i++)
+                            {
+                                // Se já tem, remove, pois é repetida
+                                if (caracter == vogaisEncontradasStream[i])
+                                {
+                                    vogaisEncontradasStream.RemoveAt(i);
+                                    vogalRemovida = true;
+                                    break;
+                                }
+                            }
+
+                            if (!vogalRemovida)
+                            {
+                                // Senão tem, adiciona
+                                vogaisEncontradasStream.Add(caracter);
+                            }
+                            
+                        }
+                        break;
+                    default:
+                        ultimoFoiConsoante = true;
+                        break;
+                }
+            }
+
+            return vogaisEncontradasStream.Count > 0 ? vogaisEncontradasStream[0] : default(char);
+        }
+    }
+}
---
+++ Stream/Stream/WordStream.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream
+{
+    public class WordStream : IStream
+    {
+        private string word = "AhJeFEKlMaKalicI";
+
+        public char getNext()
+        {
+            char carac = word[0];
+            word = word.Remove(0, 1);
+            return carac;
+        }
+
+        public bool hasNext()
+        {
+            return word.Length > 0 ? true : false;
+        }
+    }
+}
